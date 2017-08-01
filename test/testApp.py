84f24f769cb0324fd5bc99dde45bebfe1755bbfa
dcdholder <dcdholder@gmail.com>
2017-07-31 21:54:29 -0400

    Added a little output to API-level unit tests.

--- test/testApp.py
+++ test/testApp.py
@@ -15,78 +15,76 @@ class TestApp(unittest.TestCase):
 
     def testStartPositive(self):
         response = self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testPausePositive(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.post('/job/pause')
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testResumePositive(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         self.app.post('/job/pause')
         response = self.app.post('/job/resume')
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testUpdatePositive(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.get('/job/update')
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testStopPositive(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.delete('/job/stop')
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testCancelPositive(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.delete('/job/cancel')
-        self.assertTrue(status.is_success(response.status_code))
+        self.assertTrue(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testStartTwice(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.post('job/start')
-        self.assertFalse(status.is_success(response.status_code))
+        self.assertFalse(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testPausePausedJob(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         self.app.post('/job/pause')
         response = self.app.post('/job/pause')
-        self.assertFalse(status.is_success(response.status_code))
+        self.assertFalse(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testResumeRunningJob(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         response = self.app.post('/job/resume')
-        self.assertFalse(status.is_success(response.status_code))
+        self.assertFalse(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def testRequestBeforeStart(self):
-        responses = []
-        responses.append(self.app.post('/job/pause'))
-        responses.append(self.app.post('/job/resume'))
-        responses.append(self.app.get('/job/update'))
-        responses.append(self.app.delete('/job/stop'))
-        responses.append(self.app.delete('/job/cancel'))
+        responses    = []
+        requestTypes = []
+        for requestType in ['pause','resume','update','stop','cancel']:
+            responses.append(self.app.post('/job/' + requestType))
+            requestTypes.append(requestType)
 
-        for response in responses:
-            self.assertFalse(status.is_success(response.status_code))
+        for i in range(len(responses)):
+            self.assertFalse(status.is_success(responses[i].status_code), msg='Request=' + requestTypes[i] + ', Response Code=' + str(responses[i].status_code))
 
     def testRequestAfterStop(self):
         self.app.post('/job/start', data=self.startPayload, content_type=self.contentType)
         self.app.delete('/job/stop')
 
-        responses = []
-        responses.append(self.app.post('/job/pause'))
-        responses.append(self.app.post('/job/resume'))
-        responses.append(self.app.get('/job/update'))
-        responses.append(self.app.delete('/job/stop'))
-        responses.append(self.app.delete('/job/cancel'))
+        responses    = []
+        requestTypes = []
+        for requestType in ['pause','resume','update','stop','cancel']:
+            responses.append(self.app.post('/job/' + requestType))
+            requestTypes.append(requestType)
 
-        for response in responses:
-            self.assertFalse(status.is_success(response.status_code))
+        for i in range(len(responses)):
+            self.assertFalse(status.is_success(responses[i].status_code), msg='Request=' + requestTypes[i] + ', Response Code=' + str(responses[i].status_code))
 
     def testInvalidRequest(self):
         response = self.app.post('/job/starf')
-        self.assertFalse(status.is_success(response.status_code))
+        self.assertFalse(status.is_success(response.status_code), msg='Response Code=' + str(response.status_code))
 
     def tearDown(self):
         app.sortJob.stop()
